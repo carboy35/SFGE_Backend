c2b9cb3da3aebf1d9bb75c582c51433e4cb4b954
carlos pozo <hmolinet1@hotmail.com>
2018-08-11 13:01:10 -0400

    First commit

---
+++ src/main/java/com/adexus/SFGE_Backend/SfgeBackendApplication.java
@@ -0,0 +1,12 @@
+package com.adexus.SFGE_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgeBackendApplication.class, args);
+	}
+}
